d704b2ccd839c328dc7d19abfe99e32191bd10a8
William Green <=>
2021-01-28 20:52:02 -0800

    major threading update progress

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import search.searchFixed as searchFixed
 import search.searchAuction as searchAuction
 import queueManager
 import os
+import time
+from requests_html import HTMLSession
+session = HTMLSession()
+import sys
 
+'''
 def parseConf():
     if os.path.isfile("../conf.json"):
         with open("../conf.json") as confFile:
@@ -12,11 +17,77 @@ def parseConf():
     else:
         with open("../settings.json",'w') as confFile:
             print('conf file not configured; creating file')
+'''
+profiles = open("./profiles/profiles.json","r")
+searchItems = json.load(profiles)
+
+frameInterval = 1
+maxThreadCount = 8
+
+class load:
+
+    def __init__(self):
+        self._running = True
+
+    def terminate(self):
+        print('terminating')
+        self._running = False
+
+    def load(self, url):
+        global session
+        req = session.get(url)
+        results = req.html.find(".srp-results .s-item__wrapper.clearfix")
+        print('loading page')
+        self.terminate()
+        #sys.exit()
+    
+def loadSearches():
+    for itm in searchItems:
+        if threading.active_count() < maxThreadCount:
+            print(type(searchItems[0]))
+            loadItm = load()
+            t = threading.Thread(target=loadItm.load, args=(searchItems[0]['searchURL'],))
+            t.start()
+    '''
+    for itm in searchItems:
+        print("search item")
+        #processThread = threading.Thread(target=processLine, args=(dRecieved,))  # <- note extra ','
+        loadItm = load()
+        t = threading.Thread(target=loadItm.load, args=(itm['searchURL'],))
+        t.start()
+    '''
 
 if __name__ == "__main__":
     profiles = open("./profiles/profiles.json","r")
     searchItems = json.load(profiles)
+    for itm in searchItems:
+        itm['lastUpdt'] = time.time()#sets baseline timestamp for queue prioritization
+    
+    iterQueue = []
+    frmState = {"lastTime":time.time()}
+    while True:
+        #minimum interval time delay
+        time.sleep(1)
+        if time.time() - frmState['lastTime'] > frameInterval:
+            #refresh pages
+            print("refresh searches")
+            loadSearches()
+            frmState['lastTime'] = time.time()
+        
+        print(threading.active_count())
+
+        #queue stack
+        for i,itm in enumerate(iterQueue):
+            #call queue item
+            itm()
+            #delete queue item
+            del iterQueue[i]
+
     for searchItem in searchItems:
+        print(searchItem)
+        #create threads for all network requests
+        #
+        '''
         try:
             if searchItem['searchType'] == 'auction':
                 print('starting threads')
@@ -29,3 +100,4 @@ if __name__ == "__main__":
                 searchThread.start()
         except:
             pass
+        '''
