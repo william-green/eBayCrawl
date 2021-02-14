fe5f18b27bb2fe7007b6d00cf3ca9163fb542b9b
William Green <=>
2021-02-13 23:07:24 -0800

    notification revamp now with secure credentials and exception handling

--- notifs/notif.py
+++ notifs/notif.py
@@ -1,9 +1,45 @@
 import smtplib
+from datetime import datetime
+import json
 
-def send(message):
-	gmail_user = 'ebay.computer.nerd@gmail.com'
-	gmail_password = 'fuckFish42069::)'
+def send(message, log=False):
+	with open("../credentials.txt",'r') as credFile:
+		credStr = '{'+credFile.read()+'}'
+		creds = json.loads(credStr)
+		print(creds)
+	
+	gmail_user = creds['email']
+	gmail_password = creds['password']
+	sent_from = "eBay Crawler"
+	to = creds['recipients']
+	subject = ''
+	body = str(message)
+	email_text = """\
+	From: %s
+	To: %s
+	Subject: %s
 
+	%s
+	""" % (sent_from, ", ".join(to), subject, body)
+
+	#try:
+	server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+	server.ehlo()
+	server.login(gmail_user, gmail_password)
+	server.sendmail(sent_from, to, email_text)
+	server.close()
+	print('Notification sent')
+	#except:
+	#print('Problem sending notification')
+
+	if log:
+		with open("logs/log.txt","a") as logFile:
+			logFile.write(datetime.now() + ' : ' + message)
+
+	#gmail_user = 'ebay.computer.nerd@gmail.com'
+	#gmail_password = 'fuckFish42069::)'
+
+	'''
 	sent_from = 'test'
 	to = ['7078902047@vtext.com','7075135679@vtext.com']#'7075135679@vtext.com' 7078902047@vzwpix.com
 	subject = ''
@@ -26,3 +62,4 @@ def send(message):
 	print('Email sent!')
 	#except:
 	#    print('Something went wrong...')
+	'''
