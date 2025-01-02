b7cbd4e512f96b681c5198618157d90d4f3c1a01
William Green <william.green145@gmail.com>
2025-01-01 23:53:06 -0800

    db retrieval for unprocessed bin listings. critical sections protected with lock.

--- data_processing/post_process.py
+++ data_processing/post_process.py
@@ -1,13 +1,27 @@
 import queue
 import threading
-#from util.shared import OwnedEvent
 import time
+from db import db_functions as db_f
+
+def process_new_bins(db_response_data):
+    for search_frame in db_response_data:
+        print(search_frame['search']['min_price'])
+        for listing in search_frame['payload']:
+            print(listing['url'])
 
 #runs when the flag is not set
 def post_process_data(db_lock):
     while True:
         time.sleep(1)
+
+        #critical section
         db_lock.acquire()
-        #print("post_process: accessing database")
+        db_response_data = db_f.get_unprocessed_bin_listings()
         time.sleep(1)
         db_lock.release()
+        
+        process_new_bins(db_response_data)
+
+
+if __name__ == "__main__":
+    process_new_bins()
--- db/db_functions.py
+++ db/db_functions.py
@@ -55,3 +55,36 @@ def insert_bin_listing(listing: Bin_listing):
         cur.execute("INSERT INTO bin_listings (search_id, ebay_listing_id, url, accepts_best_offer, price) VALUES (?, ?, ?, ?, ?)", (listing.get_search_id(), listing.get_ebay_listing_id(), listing.get_listing_url(), listing.get_accepts_best_offer(), listing.get_price(),))
     conn.commit()
     conn.close()
+
+def get_unprocessed_bin_listings():
+    conn = sqlite3.connect(path+"db/app_data.db")
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    #fetch all listings from bin_listings that do not exist in bin_notifications
+
+    #start with fetching the search ids corresponding to the listings that have not been processed yet
+    cur.execute("SELECT DISTINCT l.search_id FROM bin_listings AS l LEFT JOIN bin_notifications AS r ON l.id = r.bin_listing_id WHERE r.id IS NULL AND l.processed=0;")
+    db_search_ids = cur.fetchall()
+
+    #[{"search":search_data, "payload":[{row_object},{row_object}...]},{...},]
+    payload = []
+    for db_search_id in db_search_ids:
+        search_id = db_search_id['search_id']
+        search_payload = []
+        cur.execute("SELECT * FROM bin_listings WHERE search_id=? AND processed=0", (search_id,))
+        db_bin_payload = cur.fetchall()
+        for db_listing in db_bin_payload:
+            search_payload.append(db_listing)
+        cur.execute("SELECT * FROM Searches WHERE id=?", (search_id,))
+        search = cur.fetchone()
+
+        payload_item = {
+            "search": search,
+            "payload": search_payload
+        }
+        payload.append(payload_item)
+    #pretty bad for update anomalies
+    cur.execute("UPDATE bin_listings SET processed=1")
+    conn.commit()
+    conn.close()
+    return payload
